77ccb9eb756280fa4e3a5449c94658ba32d39488
ignatiusobiorah <iobiorah23@gmail.com>
2024-11-06 17:04:18 +0100

    Add UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <div>
+        <UserProfile name="Pope" age="25" bio="Loves skateboarding and video games" />
         <WelcomeMessage />
         <Header />
         <MainContent />
