1517b667bdd9cd718621836faf09272efab9b30e
ignatiusobiorah <iobiorah23@gmail.com>
2024-11-15 14:06:10 +0100

    Modify Navbar.js

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,12 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav style={{border: "2px solid orange"}}>
+
+    <nav style={{
+      border: "2px solid orange",
+      backgroundColor: "lightblue", 
+      display: "flex", 
+      justifyContent: "space-around" 
+    }}>
         <Link style={{padding: "20px"}} to="/">Home</Link>
         <Link style={{padding: "20px"}}  to="/about">About</Link>
         <Link style={{padding: "20px"}}  to="/services">Services</Link>
         <Link style={{padding: "20px"}}  to="/contact">Contact</Link>
     </nav>
+    
+
   )
 }
 
