73ddc8e0cca162ff4948b5064d4defd8cbd6e0f2
ignatiusobiorah <iobiorah23@gmail.com>
2024-12-02 14:08:10 +0100

    Modify Homepage.jsx

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -17,22 +17,24 @@ function Homepage() {
     }, []);
 
   return (
-    <div className="p-4 md:p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    {recipes.map((recipe, index) => (
-      <div
-        key={index}
-        className="bg-white rounded-lg shadow-md p-4 hover:shadow-xl transition-shadow duration-300 ease-in-out"
-      >
-        <img
-          src={recipe.image}
-          alt={recipe.title}
-          className="w-full h-48 object-cover rounded-t-lg"
-        />
-        <h2 className="text-xl font-semibold mt-2">{recipe.title}</h2>
-        <p className="text-gray-600 text-base">{recipe.summary}</p>
-      </div>
-    ))}
-  </div>
+    <div className="p-4 sm:p-6 md:p-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {recipes.map((recipe, index) => (
+        <div
+            key={index}
+            className="bg-white rounded-lg shadow-md p-4 sm:p-6 md:p-8 hover:shadow-xl transition-shadow duration-300 ease-in-out"
+        >
+            <img
+            src={recipe.image}
+            alt={recipe.title}
+            className="w-full h-48 sm:h-64 md:h-48 object-cover rounded-t-lg"
+            />
+            <h2 className="text-lg sm:text-xl md:text-2xl font-semibold mt-2">
+            {recipe.title}
+            </h2>
+            <p className="text-gray-600 text-base sm:text-lg">{recipe.summary}</p>
+        </div>
+        ))}
+    </div>
   );
 }
 
