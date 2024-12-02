c8876e1ed49cdabe7b05c8713f85a715be610121
ignatiusobiorah <iobiorah23@gmail.com>
2024-12-02 12:44:10 +0100

    modify tailwind.config,js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,18 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkmode: false,
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
+  variants: {extend: {}},
 }
 
-module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkmode: false,
-  theme: {extend: {}},
-  variants: {extend: {}},
-  plugins: [],
-}
 
 
